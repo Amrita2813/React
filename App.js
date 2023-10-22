ec3e85b77dc5c127d000fcd435153197da46f3db
amrita2813 <amrita.kaur@coditas.com>
2023-10-22 18:57:05 +0530

    initialised npm and installed parcel , react , react-dom

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement("div", { id: "parent" },
 
-[  React.createElement("div", { id: "child1" },
+[  React.createElement("div", { id: "child1" }, 
      [React.createElement("h1", {}, "Hello React h1 tag"),
       React.createElement("h2", {}, "Hello React h2 tag")]
     ),  React.createElement("div", { id: "child2" },
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Basic React Concepts
+
+
+# Parcel 
+ - Dev Build
+ - local server
+ - HMR - hot module replacement (reload or refreshed the page)
+ - File watching Algorithm - written in c++
+ - Builds faster because of caching
